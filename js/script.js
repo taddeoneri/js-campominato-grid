44a3ccd6b52daa5a4f837e86e33225dba4b004e4
taddeoneri <taddeo.neri10@gmail.com>
2023-03-17 16:11:34 +0100

    init js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+
+
+const formSelect = document.getElementById('formSelect');
+formSelect.addEventListener('click', play);
+
+function createSquare(squareNumbers){
+    const table = document.getElementById('table');
+    table.innerHTML = 
+    `
+        <div class="d-flex justify-content-center align-items-center "></div>
+    `;
+    table.style.width = `calc(100% &{})`
+}
