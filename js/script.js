f7f81e65d0743a8d1c50e7291ee9b898e80aec3d
taddeoneri <taddeo.neri10@gmail.com>
2023-03-17 16:43:54 +0100

    select level

--- js/script.js
+++ js/script.js
@@ -4,11 +4,31 @@
 const formSelect = document.getElementById('formSelect');
 formSelect.addEventListener('click', play);
 
-function createSquare(squareNumbers){
+function createSquare(i, row){
+    const square = document.createElement('div');
+    square.classList.add('square')
+    square.style.width = `calc(100% ${row})`;
+    square.style.height = `calc(100% ${row})`;
+    return square;
+}
+
+function play(e){
+    e.preventDefault();
     const table = document.getElementById('table');
-    table.innerHTML = 
-    `
-        <div class="d-flex justify-content-center align-items-center "></div>
-    `;
-    table.style.width = `calc(100% &{})`
+    table.innerHTML = '';
+    const level = document.getElementById('select').value;
+    let squareNumbers;
+    if(level === 'Easy'){
+        squareNumbers = 100;
+    } else if(level === 'Medium'){
+        squareNumbers = 81;
+    } else{
+        squareNumbers = 49;
+    }
+    console.log(squareNumbers);
+    console.log(level);
+    let row = Math.sqrt(squareNumbers);
+    for(let i = 1; i <= squareNumbers; i++){
+        table.appendChild(createSquare(i, row));
+    }
 }
